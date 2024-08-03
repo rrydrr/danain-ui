de19fba1dc95e091159fede79cf6761261af8326
rrydrr <135146023+rrydrr@users.noreply.github.com>
2024-08-03 16:21:38 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# danain_ui
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(myApp());
+}
+
+MaterialApp myApp() {
+return MaterialApp(
+	home: Scaffold(
+	  appBar: AppBar(
+	  	title: const Text('TITLE'),
+      leading: const ImageIcon(AssetImage('assets/Danain.png')),
+      actions: [const Icon(Icons.notifications)],
+	  ), 
+	  body: const Center(
+	  	child: Text(
+	  	'BODY',
+	  	style: TextStyle(fontSize: 24),
+	  	),
+	  ),
+	), 
+	debugShowCheckedModeBanner: false, //Removing Debug Banner
+);
+}
